47b24396d8c39e89166747cb7733e7ab6e1b182b
Uladzislau <uladzislau.plitok@leverx.com>
2023-06-19 15:48:41 +0200

    add start command to package.json, add class to launch app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log(process.argv)
+import proccess from 'process'
+import {getUsernameFromArgs} from './utils/index.js'
+
+class App {
+    constructor() {
+        this.userName = getUsernameFromArgs()
+        this.handleExit()
+    }
+
+    welcomeUser() {
+        const welcomeMessage = `Welcome to the File Manager, ${this.userName}!`
+        this._sendMessageToConsole(welcomeMessage)
+    }
+
+    _sendMessageToConsole(message) {
+        proccess.stdout.write(message + '\n')
+    }
+
+    handleExit() {
+        proccess.on('exit',()=>{
+           console.log('exit')
+       })
+    }
+}
+
+const app = new App()
+app.welcomeUser()
---
+++ src/utils/index.js
@@ -0,0 +1,8 @@
+export const getUsernameFromArgs = () => {
+    const USERNAME_FLAG = '--username';
+    const userNameStr = process.argv.find((arg) => arg.includes('--username'))
+    if(userNameStr){
+        return userNameStr.split('=')[1]
+    }
+    throw new Error('You should pass username as argument')
+}
