8c5e798964d3ab58454b551e3467fcd96af9adc5
Uladzislau <uladzislau.plitok@leverx.com>
2023-06-20 17:55:53 +0200

    add init path setup, change name of init App method

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
 import process from 'node:process';
 import {getUsernameFromArgs} from './utils/index.js'
 import {EventsModule} from "./modules/events.module.js";
+import {MessagesService} from './modules/messages.service.js'
+import os from "os";
 
 class App {
     constructor() {
         this.userName = getUsernameFromArgs()
         this.eventHandler = new EventsModule(this.userName)
+        this.messageService = new MessagesService()
 
         this.eventHandler.setUpListeners()
     }
 
-    welcomeUser() {
-        const welcomeMessage = `Welcome to the File Manager, ${this.userName}!`
-        this._sendMessageToConsole(welcomeMessage)
+    appInit() {
+        this._setInitialPath()
+        this._welcomeUser()
     }
 
-    _sendMessageToConsole(message) {
-        process.stdout.write(message + '\n')
+    _welcomeUser() {
+        const welcomeMessage = `Welcome to the File Manager, ${this.userName}!`
+        const currentPath = process.cwd()
+        const pathMessage = `You are currently in ${currentPath}`
+        this.messageService.sendMessage(`${welcomeMessage}\n${pathMessage}`)
+    }
+
+    _setInitialPath() {
+        const initPath = os.homedir()
+        try {
+            process.chdir(initPath)
+        } catch (e) {
+            throw new Error('Whooops')
+        }
     }
 }
 
 const app = new App()
-app.welcomeUser()
+app.appInit()
